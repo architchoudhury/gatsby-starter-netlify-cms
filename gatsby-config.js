a69d6dbbb9e49d1bc574e3e9f261d076f5e3a423
Archit Choudhury <architchoudhury@outlook.com>
2019-07-03 14:58:47 +0800

    Modified Navbar and changed some default boilerplate text

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'FluentUp | Blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'FluentUp is an online language learning marketplace that connects accredited language schools with aspiring students from all over the world.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
